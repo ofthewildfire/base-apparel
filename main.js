7b88c67ce9df44b166b162f41745aba7f3fcb00a
ofthewildfire <103148299+ofthewildfire@users.noreply.github.com>
2024-02-21 20:42:50 +0200

    delete pre

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ import './style.css';
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 
+const emailInput = document.querySelector("input[type='email']")
+const btn = document.querySelector("")
+if (!emailInput.value) {
+    console.log("Invalid")
+} else {
+    console.log("Success!");
+}
