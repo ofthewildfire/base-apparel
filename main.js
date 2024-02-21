449f8d8dbd6a0e000eec5118486b4f043f8679d9
ofthewildfire <103148299+ofthewildfire@users.noreply.github.com>
2024-02-21 09:28:45 +0200

    Files for challenge

---
+++ main.js
@@ -0,0 +1,7 @@
+import '@fontsource-variable/josefin-sans';
+import "./modernreset.css";
+import './style.css';
+// Supports weights 100-700
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+
