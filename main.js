aa0c2ece3b8457275d2e7ae3978ce1b73f10aaaa
ofthewildfire <103148299+ofthewildfire@users.noreply.github.com>
2024-02-24 03:41:18 +0200

    so far

--- main.js
+++ main.js
@@ -5,3 +5,27 @@ import './style.css';
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 
+const emailRegex = `^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$`
+
+const emailInput = document.getElementById("email")
+const submitBtn = document.querySelector(".form-btn")
+const errorIcon = document.querySelector(".form-error")
+const errorText = document.querySelector(".form-error-text")
+
+submitBtn.addEventListener("click", validate)
+
+function validate(e){
+    e.preventDefault()
+    const email = document.getElementById('email').value
+    const regex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
+    if(regex.test(email)) {
+      console.log("Valid email!")
+      errorIcon.classList.add("hidden")
+      errorText.classList.add("hidden")
+      emailInput.value = '';
+    } else {
+      console.log("Invalid email!")
+        errorIcon.classList.remove("hidden")
+        errorText.classList.remove("hidden")
+    }
+  }
